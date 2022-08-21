3c00273cad92cee18460f51c8b4a861a3a4b0700
justDaBuK <kevinjust87@gmail.com>
2022-08-21 10:31:42 +0200

    feature - show map information

--- src/views/SessionDetails.vue
+++ src/views/SessionDetails.vue
@@ -9,9 +9,21 @@
     >
       <span v-if="index === participantList.length - 1"> and </span>
       {{ participantId
-      }}<span v-if="index !== participantList.length - 1">,</span>
+      }}<span
+        v-if="
+          index !== participantList.length - 1 &&
+          index !== participantList.length - 2
+        "
+        >,</span
+      >
     </router-link>
     <span> are joining the game</span>
+    <h2>Current matching</h2>
+    <ul>
+      <li v-for="[giver, receiver] in participantMap" :key="giver">
+        {{ giver }} -> {{ receiver }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -35,19 +47,28 @@ export default defineComponent({
   },
   computed: {
     startDate(): Date {
+      // for some reason, the string isn't properly deserialized,
+      // therefore, we explicitly create a Date out of it
       return new Date(this.session.start)
     },
     startMonth(): string {
       return MONTH_NAMES[this.startDate.getMonth()]
     },
     endDate(): Date {
+      // for some reason, the string isn't properly deserialized,
+      // therefore, we explicitly create a Date out of it
       return new Date(this.session.end)
     },
     endMonth(): string {
       return MONTH_NAMES[this.endDate.getMonth()]
     },
+    participantMap(): Map<string, string> {
+      // for some reason, the object isn't properly deserialized,
+      // therefore, we explicitly create a map out of it
+      return new Map(Object.entries(this.session.participantMap))
+    },
     participantList(): string[] {
-      return Array.from(this.session.participantMap.keys())
+      return Array.from(this.participantMap.keys())
     }
   },
   created() {
